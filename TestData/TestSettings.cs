aa525a4919f5b4b011ef11102ca696264a09610b
OlegMongolCyberVersion <100905218+OlegMongolCyberVersion@users.noreply.github.com>
2022-06-25 19:50:03 +0300

    Add files via upload

---
+++ TestData/TestSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWebShop
+{
+    public class TestSettings
+    {
+        public static string HostPrefixURL = "http://demowebshop.tricentis.com";
+        public static string URL = "http://demowebshop.tricentis.com/";
+    }
+}
---
+++ TestData/UserNameForTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWebShop
+{
+    internal class UserNameForTests
+    {
+        public static string gender { get; set; } = "male";
+        public static string firstName { get; set; } = "Александр";
+        public static string lastName { get; set; } = "Потапов";
+        public static string email { get; set; } = "AleksandrPotapov01.01.1992@gmail.com";
+        public static string pasword { get; set; } = "AleksandrPotapov1992";
+        public static string confirmPassword { get; set; } = "AleksandrPotapov1992";
+    }
+}
